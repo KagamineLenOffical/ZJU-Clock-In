7244daf18a83017f45bf27d52981a74e4fa26435
KagamineLenOffical <61376101+KagamineLenOffical@users.noreply.github.com>
2022-12-19 15:28:39 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -75,7 +75,6 @@ class ClockIn(object):
             html = res.content.decode()
 
         try:
-            print(html)
             old_infos = re.findall(r'oldInfo: ({[^\n]+})', html)
             if len(old_infos) != 0:
                 old_info = json.loads(old_infos[0])
